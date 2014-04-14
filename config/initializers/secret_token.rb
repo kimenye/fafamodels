d7c508bf94ca8213e90977e437771d322419d799
Sir Bertly <sirbertly@touchinspiration.com>
2014-04-14 13:22:23 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fafamodels::Application.config.secret_key_base = '65379bcfe2030a13dc641f419374df5be5eb463c4df9df3396e959d3023f2c309cb54980bca6bb1e3661e5af0c4bf634ceb2fafbee7cfcd50a86e4d729e8c6b4'
